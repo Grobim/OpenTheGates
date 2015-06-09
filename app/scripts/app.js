23263c5f342cd4873b108a86959dd237485616e8
Nicolas Chalet <nicolas.chalet@gmail.com>
2015-06-10 01:33:35 +0200

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name sfeirCampPortalApp
+ * @description
+ * # sfeirCampPortalApp
+ *
+ * Main module of the application.
+ */
+angular.module('sfeirCampPortalApp', [
+    'firebase',
+    'firebase.ref',
+    'firebase.auth'
+  ]);
