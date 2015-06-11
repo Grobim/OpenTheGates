8ff49abeab74caa0645677fa385f8c5a2ddf85d3
Nicolas Chalet <nicolas.chalet@gmail.com>
2015-06-11 03:41:51 +0200

    authLayout ok

--- app/sfeirCampPortal/layout/layout.controller.js
+++ app/sfeirCampPortal/layout/layout.controller.js
@@ -2,14 +2,15 @@
   'use strict';
 
   angular.module('sfeirCampPortalApp.layout')
-    .controller('LayoutController', ['$state', 'headerStates', LayoutController])
+    .controller('LayoutController', ['$state', 'headerStates', 'Auth', LayoutController])
     ;
 
-  function LayoutController($state, headerStates) {
+  function LayoutController($state, headerStates, Auth) {
     var _this = this;
 
     _this.goToState = goToState;
     _this.isCurrentState = isCurrentState;
+    _this.userHasAuthRights = userHasAuthRights;
 
     (function init() {
 
@@ -25,6 +26,18 @@
 
     function isCurrentState(stateName) {
       return $state.includes(stateName);
+    }
+
+    function userHasAuthRights(state) {
+      if (_.isBoolean(state.auth)) {
+        var authData = Auth.$getAuth();
+        if (authData) {
+          return state.auth === true;
+        } else {
+          return state.auth === false;
+        }
+      }
+      return true;
     }
 
   }
